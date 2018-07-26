83256e84f3a6ad02292df8d8c7b0b1d29841d3e4
eduardomassami <massami.uchida@gmail.com>
2018-07-26 19:21:35 -0300

    parei na aula 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad';
+  luke  =  {name: 'Luke', isJedi: true, temple: 'Coruscant'}
+  han   =  {name: 'Han Solo', isJedi: false}
+  leia  =  {name: 'Leia', isJedi: false}
 }
